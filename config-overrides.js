63ff9083ad2fefba39861181adaf78dd71b127cc
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-11-11 18:23:24 +0100

    Modify ESLint config with customize-cra.

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,21 @@
-const rewireEslint = require('react-app-rewire-eslint');
+const { override, useEslintRc } = require('customize-cra');
 
-module.exports = config => rewireEslint(config);
+const useEslintTypescript = () => config => {
+  const eslintRule = config.module.rules.filter(
+    r => r.use && r.use.some(u => u.options && u.options.useEslintrc !== void 0)
+  )[0];
+
+  eslintRule.test = /\.[j,t]sx?/
+
+  const rules = config.module.rules.map(
+    r =>
+      r.use && r.use.some(u => u.options && u.options.useEslintrc !== void 0)
+        ? eslintRule
+        : r
+  );
+  config.module.rules = rules;
+
+  return config;
+}
+
+module.exports = override(useEslintRc(), useEslintTypescript());
