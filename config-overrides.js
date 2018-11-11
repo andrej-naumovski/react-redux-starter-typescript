0c64b3b61adb57a47b19db135dd18c2641f79b9a
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-11-11 20:24:47 +0100

    Replace custom Typescript override with one from customize-cra.

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,3 @@
-const { override, useEslintRc } = require('customize-cra');
+const { override, useEslintRc, enableEslintTypescript } = require('customize-cra');
 
-const useEslintTypescript = () => config => {
-  const eslintRule = config.module.rules.filter(
-    r => r.use && r.use.some(u => u.options && u.options.useEslintrc !== void 0)
-  )[0];
-
-  eslintRule.test = /\.[j,t]sx?/
-
-  const rules = config.module.rules.map(
-    r =>
-      r.use && r.use.some(u => u.options && u.options.useEslintrc !== void 0)
-        ? eslintRule
-        : r
-  );
-  config.module.rules = rules;
-
-  return config;
-}
-
-module.exports = override(useEslintRc(), useEslintTypescript());
+module.exports = override(useEslintRc(), enableEslintTypescript());
